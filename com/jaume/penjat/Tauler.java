b5e8ef77b4cf7c35f1d890859c3c305a83777827
JL Cardona <jcardona@cifpfbmoll.eu>
2021-02-10 17:22:15 +0100

    //commit nuevo Tauler

--- com/jaume/penjat/Tauler.java
+++ com/jaume/penjat/Tauler.java
@@ -3,6 +3,7 @@ package com.jaume.penjat;
 public class Tauler {
     char[] paraulaSecreta;
     int intents;
+    String[] paraulaEndevinada;
 
     public Tauler(){
 
@@ -15,5 +16,21 @@ public class Tauler {
     }
     public int getIntents(){
         return intents;
+    }
+    public String verificar(String lletra){
+        for (int i = 0; i < paraulaSecreta.length(); i++){
+
+        }
+        if(lletra.length() != 1){
+            return "Lletra incorrecte";
+        }
+        else {
+            return "Lletra correcte";
+        }
+
+
+    }
+    public String[] getPalabraEndevinada(){
+        return paraulaEndevinada;
     }
 }
